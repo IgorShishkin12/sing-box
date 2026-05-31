4dda5b1502085d2f02af5c0c4a47f3e0f5186d64
IgorShishkin12 <qwertyshishkin@yandex.ru>
2026-05-31 07:20:55 +0300

    fix configs (container names were changed in previous commit, but not hostnames in configs) also fixing runner.sh a little bit, mostly styling changes

--- e2e/runner.sh
+++ e2e/runner.sh
@@ -21,17 +21,24 @@ $COMPOSE_CMD -f docker-compose.tcp.yml build
 run_test() {
     local file="$1"
     local exit_from="$2"
+    rc=0
     echo ""
     echo "=== Running: $file (exit-from: $exit_from) ==="
     $COMPOSE_CMD -f "$file" up \
         --exit-code-from "$exit_from" \
-        --abort-on-container-exit
-    $COMPOSE_CMD -f "$file" down --volumes
+        --abort-on-container-exit || rc=1
+    $COMPOSE_CMD -f "$file" down
+    if [ $rc -ne 0 ]; then
+        echo "Some E2E tests failed"
+        exit 1
+    fi
 }
 
+run_test docker-compose.simple-tcp.yml  e2e-client
+run_test docker-compose.length-test.yml  e2e-client
 run_test docker-compose.tcp.yml  e2e-client
-run_test docker-compose.udp.yml  e2e-client-udp
-run_test docker-compose.auto.yml e2e-client-auto
+run_test docker-compose.udp.yml  e2e-client
+run_test docker-compose.auto.yml e2e-client
 
 echo ""
 echo "=== ALL E2E TESTS PASSED ==="
