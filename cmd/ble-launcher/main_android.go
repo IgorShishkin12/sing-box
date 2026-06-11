d94170934756a71c977a67c62ef2de8b602034d9
IgorShishkin12 <qwertyshishkin@yandex.ru>
2026-06-12 00:26:16 +0300

    fix ??

--- cmd/ble-launcher/main_android.go
+++ cmd/ble-launcher/main_android.go
@@ -1,74 +1,86 @@
 //go:build android && with_reticulum
 
-// Package main compiles as a JNI shared library (-buildmode c-shared) for Android.
-// It exposes three JNI methods that a minimal Android Service calls to run the
-// Reticulum bridge with BLE support.
-//
-// Build:
-//
-//	GOOS=android GOARCH=arm64 CGO_ENABLED=1 CC=<ndk-clang> \
-//	  go build -buildmode c-shared -tags "with_reticulum" \
-//	  -o libsing-box-ble.so ./cmd/ble-launcher/
 package main
 
 /*
 #include <jni.h>
+#include <android/log.h>
 #include <stdlib.h>
 #include "../../bridge/include/reticulum_bridge.h"
 
-// Extract JavaVM* from a live JNIEnv*. Used to pass to reticulum_set_jvm on
-// the calling Java thread so btleplug's Adapter::new() has a bound JNIEnv.
 static void* extract_jvm(JNIEnv* env) {
     JavaVM* jvm = NULL;
     (*env)->GetJavaVM(env, &jvm);
     return (void*)jvm;
 }
-
 static const char* jni_get_utf(JNIEnv* env, jstring s) {
     return (*env)->GetStringUTFChars(env, s, NULL);
 }
 static void jni_release_utf(JNIEnv* env, jstring s, const char* c) {
     (*env)->ReleaseStringUTFChars(env, s, c);
 }
+static void do_log(int prio, const char* msg) {
+    __android_log_print(prio, "sing-box-ble", "%s", msg);
+}
 */
 import "C"
 import (
+	"context"
+	"fmt"
 	"unsafe"
 
-	"github.com/sagernet/sing-box/log"
+	sblog "github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/protocol/reticulum"
 )
 
 func main() {}
 
-// Java_com_singbox_ble_Bridge_nativeSetJVM passes the JavaVM to the Rust
-// bridge and triggers btleplug platform initialisation on this Java thread.
-// Must be called from Service.onCreate() before nativeInit.
-//
-//export Java_com_singbox_ble_Bridge_nativeSetJVM
-func Java_com_singbox_ble_Bridge_nativeSetJVM(env *C.JNIEnv, cls C.jobject) {
-	jvm := C.extract_jvm(env)
-	reticulum.BridgeSetJVM(unsafe.Pointer(jvm))
+// logcatLogger implements log.ContextLogger by writing to Android logcat.
+type logcatLogger struct{}
+
+func (logcatLogger) emit(prio C.int, args ...any) {
+	s := fmt.Sprint(args...)
+	cs := C.CString(s)
+	C.do_log(prio, cs)
+	C.free(unsafe.Pointer(cs))
 }
 
-// Java_com_singbox_ble_Bridge_nativeInit initialises the bridge with the
-// given JSON config. Returns 0 on success, -1 on error.
-//
+func (l logcatLogger) Trace(a ...any)                               { l.emit(C.ANDROID_LOG_VERBOSE, a...) }
+func (l logcatLogger) Debug(a ...any)                               { l.emit(C.ANDROID_LOG_DEBUG, a...) }
+func (l logcatLogger) Info(a ...any)                                { l.emit(C.ANDROID_LOG_INFO, a...) }
+func (l logcatLogger) Warn(a ...any)                                { l.emit(C.ANDROID_LOG_WARN, a...) }
+func (l logcatLogger) Error(a ...any)                               { l.emit(C.ANDROID_LOG_ERROR, a...) }
+func (l logcatLogger) Fatal(a ...any)                               { l.emit(C.ANDROID_LOG_FATAL, a...) }
+func (l logcatLogger) Panic(a ...any)                               { l.emit(C.ANDROID_LOG_FATAL, a...) }
+func (l logcatLogger) TraceContext(_ context.Context, a ...any)     { l.Trace(a...) }
+func (l logcatLogger) DebugContext(_ context.Context, a ...any)     { l.Debug(a...) }
+func (l logcatLogger) InfoContext(_ context.Context, a ...any)      { l.Info(a...) }
+func (l logcatLogger) WarnContext(_ context.Context, a ...any)      { l.Warn(a...) }
+func (l logcatLogger) ErrorContext(_ context.Context, a ...any)     { l.Error(a...) }
+func (l logcatLogger) FatalContext(_ context.Context, a ...any)     { l.Fatal(a...) }
+func (l logcatLogger) PanicContext(_ context.Context, a ...any)     { l.Panic(a...) }
+
+// compile-time check that logcatLogger satisfies the interface
+var _ sblog.ContextLogger = logcatLogger{}
+
+//export Java_com_singbox_ble_Bridge_nativeSetJVM
+func Java_com_singbox_ble_Bridge_nativeSetJVM(env *C.JNIEnv, cls C.jobject) {
+	reticulum.BridgeSetJVM(unsafe.Pointer(C.extract_jvm(env)))
+}
+
 //export Java_com_singbox_ble_Bridge_nativeInit
 func Java_com_singbox_ble_Bridge_nativeInit(env *C.JNIEnv, cls C.jobject, configJSON C.jstring) C.jint {
 	cstr := C.jni_get_utf(env, configJSON)
 	defer C.jni_release_utf(env, configJSON, cstr)
 	cfg := C.GoString((*C.char)(unsafe.Pointer(cstr)))
 
-	reticulum.BridgeSetLogger(log.NewNOPFactory().Logger())
+	reticulum.BridgeSetLogger(logcatLogger{})
 	if err := reticulum.BridgeInit(cfg); err != nil {
 		return -1
 	}
 	return 0
 }
 
-// Java_com_singbox_ble_Bridge_nativeShutdown tears down the bridge.
-//
 //export Java_com_singbox_ble_Bridge_nativeShutdown
 func Java_com_singbox_ble_Bridge_nativeShutdown(env *C.JNIEnv, cls C.jobject) {
 	reticulum.BridgeShutdown()
--- protocol/reticulum/bridge_stub_reticulum.go
+++ protocol/reticulum/bridge_stub_reticulum.go
@@ -6,10 +6,10 @@ package reticulum
 #cgo CFLAGS: -I${SRCDIR}/../../bridge/include
 #cgo !android LDFLAGS: ${SRCDIR}/../../bridge/target/release/libsing_box_reticulum_bridge.a -lpthread -ldl -lm
 #cgo linux,!android LDFLAGS: -ldbus-1
-#cgo android,arm64 LDFLAGS: ${SRCDIR}/../../bridge/target/aarch64-linux-android/release/libsing_box_reticulum_bridge.a -lm
-#cgo android,arm   LDFLAGS: ${SRCDIR}/../../bridge/target/armv7-linux-androideabi/release/libsing_box_reticulum_bridge.a -lm
-#cgo android,386   LDFLAGS: ${SRCDIR}/../../bridge/target/i686-linux-android/release/libsing_box_reticulum_bridge.a -lm
-#cgo android,amd64 LDFLAGS: ${SRCDIR}/../../bridge/target/x86_64-linux-android/release/libsing_box_reticulum_bridge.a -lm
+#cgo android,arm64 LDFLAGS: ${SRCDIR}/../../bridge/target/aarch64-linux-android/release/libsing_box_reticulum_bridge.a -lm -llog
+#cgo android,arm   LDFLAGS: ${SRCDIR}/../../bridge/target/armv7-linux-androideabi/release/libsing_box_reticulum_bridge.a -lm -llog
+#cgo android,386   LDFLAGS: ${SRCDIR}/../../bridge/target/i686-linux-android/release/libsing_box_reticulum_bridge.a -lm -llog
+#cgo android,amd64 LDFLAGS: ${SRCDIR}/../../bridge/target/x86_64-linux-android/release/libsing_box_reticulum_bridge.a -lm -llog
 #include "reticulum_bridge.h"
 #include <stdlib.h>
 extern void goOnLog    (uint8_t level,        char*       target,    char*  message);
